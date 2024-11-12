a86f6bfc87537576629d31e2942bb474923c3dfa
thithada <150765761+thithada@users.noreply.github.com>
2024-11-12 12:15:39 +0700

    สร้าง blog slug
    ก็ทำงานส่งต้องใช้ blog slug ยังไม่เข้าใจ

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import exp from "constants";
+import { useParams } from "next/navigation";
+
+const BlogPost = () => {
+    const params = useParams();
+    const slug = params?.slug as string;
+
+    const formattext = slug.replace(/-/g," ")
+
+    return (
+        <p className="text text-2xl ">
+            มื้อนี่เราก็จะมาโชว์ blog ที่ดึงจาก slug เน้อ <strong className="bg-cyan-300">{formattext}</strong>
+        </p>
+    );
+}
+
+export default BlogPost;
---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function BlogPage(){
+    return (
+      <>
+        <div>
+          <h1 >หีใหย่</h1>
+        </div>
+      </>
+    )
+  }
